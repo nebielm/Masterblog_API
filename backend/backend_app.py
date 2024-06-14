759a50f6571a173a05550abe2403cbdb49d3e4b3
Nebiel Mohammed <nebielmohammedhotmail.com@Air-von-Nebiel.fritz.box>
2024-06-14 22:13:34 +0200

    added add endpoint

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+from random import randint
 
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
@@ -10,9 +11,37 @@ POSTS = [
 ]
 
 
+def validate_book_data(data):
+    if "title" not in data or "content" not in data:
+        return False
+    return True
+
+
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
     return jsonify(POSTS)
+
+
+@app.route('/api/posts', methods=['POST'])
+def add_posts():
+    data = request.get_json()
+    if not validate_book_data(data):
+        return jsonify({"error": "Invalid book data"}), 400
+    title = data["title"]
+    content = data["content"]
+    id_is_unique = False
+    while not id_is_unique:
+        id = randint(1, 1000)
+        id_is_unique = True
+        for post in POSTS:
+            if post['id'] == id:
+                id_is_unique = False
+                break
+    new_post = {"id": id, "title": title, "content": content}
+    POSTS.append(new_post)
+    return jsonify(new_post), 201
+
+
 
 
 if __name__ == '__main__':
